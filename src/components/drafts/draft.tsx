e978d486eecb65a5d38e922965b6dc501854285f
swaraj bachu <rajswaraj.r@gmail.com>
2023-08-28 01:52:15 +0530

    some minor updates

--- src/components/drafts/draft.tsx
+++ src/components/drafts/draft.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { BsFillCalendarFill } from "react-icons/bs";
 import { api } from "~/utils/api";
 import LogoLoad from "../custom/loading/logoLoad";
@@ -11,7 +12,7 @@ const DraftPage = () => {
   if (error) return <div>Something Went Wrong</div>;
 
   return (
-    <div className="flex w-full flex-col">
+    <div className=" relative flex w-full flex-col">
       <div className="flex justify-between">
         <h2 className="text-2xl font-bold">Draft</h2>
         <div className="flex gap-2">
@@ -27,20 +28,26 @@ const DraftPage = () => {
         xl:grid-cols-3
         "
       >
-        {data
-          ? // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-            data.map((item) => (
-              <PostCard
-                key={item.id}
-                id={item.id}
-                content={item.defaultContent}
-                refetch={refetch}
-              />
-            ))
-          : null}
-        {/* <PostCard id="1" />
-        <PostCard id="2" />
-        <PostCard id="3" /> */}
+        {data.length > 0 ? (
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          data.map((item) => (
+            <PostCard
+              key={item.id}
+              id={item.id}
+              content={item.defaultContent}
+              refetch={refetch}
+            />
+          ))
+        ) : (
+          <div className="absolute grid top-[40dvh] left-[40%] place-content-center">
+            <h1 className="text-center text-xl mb-2 font-semibold">
+              No Drafts Available
+            </h1>
+            <Link href="/newpost" className="btn">
+              Create New Draft
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
