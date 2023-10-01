ff922f7b8bc0ac38d120ebe6572ba5f3399cb466
swaraj bachu <rajswaraj.r@gmail.com>
2023-10-02 00:40:53 +0700

    favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,16 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@material-tailwind/react";
 import { Analytics } from "@vercel/analytics/react";
+import { type Metadata, type NextPage } from "next";
 import { type AppProps, type AppType } from "next/app";
-import { Toaster } from "react-hot-toast";
-import { api } from "~/utils/api";
-
-import { ClerkProvider } from "@clerk/nextjs";
-import { type NextPage } from "next";
 import { Router } from "next/router";
 import { useEffect, useState, type ReactElement, type ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
 import LogoLoad from "~/components/custom/loading/logoLoad";
 import Lenswrapper from "~/components/wrappers/lenswrapper";
 import "~/styles/calendar.css";
 import "~/styles/globals.css";
+import { api } from "~/utils/api";
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -21,12 +20,45 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+export const metadata: Metadata = {
+  title: "Aperturs",
+  description: "One Stop Social Media Management Software",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/favicon_light.ico",
+        href: "/favicon_light.ico",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/favicon_dark.ico",
+        href: "/favicon_dark.ico",
+      },
+    ],
+  },
+};
+
+const getFaviconPath = (isDarkMode = false) => {
+  return `/favicon_${isDarkMode ? "light" : "dark"}.ico`;
+};
+
 const MyApp: AppType = ({
   Component,
   pageProps: { ...pageProps },
 }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
   const [loading, setLoading] = useState(false);
+  const [faviconHref, setFaviconHref] = useState("/favicon_light.ico");
+
+  useEffect(() => {
+    // Get current color scheme.
+    const matcher = window.matchMedia("(prefers-color-scheme: dark)");
+    // Set favicon initially.
+    setFaviconHref(getFaviconPath(matcher.matches));
+    // Change favicon if the color scheme changes.
+    matcher.onchange = () => setFaviconHref(getFaviconPath(matcher.matches));
+  }, [faviconHref]);
 
   useEffect(() => {
     const start = () => setLoading(true);
@@ -51,13 +83,14 @@ const MyApp: AppType = ({
 
   return (
     <Lenswrapper>
-      <ClerkProvider {...pageProps}
-      
-      appearance={{
-        elements: {
-         formButtonPrimary: 'bg-primary'
-        }
-      }}>
+      <ClerkProvider
+        {...pageProps}
+        appearance={{
+          elements: {
+            formButtonPrimary: "bg-primary",
+          },
+        }}
+      >
         <ThemeProvider>
           <Toaster position="top-left" reverseOrder={false} />
           {getLayout(
