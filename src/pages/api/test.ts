651ef7bbe78dd2a14a6fc3d8437faee0a9f04616
swaraj bachu <rajswaraj.r@gmail.com>
2023-10-06 16:51:29 +0700

    test

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,9 +1,19 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
+import { prisma } from "~/server/db";
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const seomthing = req.body;
+    
+    await prisma.test.create({
+        data: {
+           // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+           data: `${seomthing}`
+        },
+    })
     res.status(200).json({ message: "OKAY" });
 }
