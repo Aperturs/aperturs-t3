095cb0f5347702edb781c2df3730ff4580937ee6
swaraj bachu <rajswaraj.r@gmail.com>
2023-10-06 18:59:12 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,20 @@ const config = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
+  async headers() {
+    return [
+        {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+},
   transpilePackages: ["@lens-protocol"],
   async redirects() {
     return [
