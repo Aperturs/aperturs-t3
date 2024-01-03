372bcce5187f0d5e6ded30788f936745cd9cad3c
swaraj bachu <rajswaraj.r@gmail.com>
2024-01-03 19:08:05 +0530

    removed depricated zustand create and changed to createWithEqualityFn

--- src/store/post-store.ts
+++ src/store/post-store.ts
@@ -1,4 +1,5 @@
-import { create } from "zustand";
+import { shallow } from "zustand/shallow";
+import { createWithEqualityFn } from "zustand/traditional";
 
 type StateValues = {
   defaultContent: string;
@@ -29,15 +30,19 @@ const initialState: StateValues = {
   content: [],
 };
 
-export const useStore = create<State>((set) => ({
-  ...initialState,
-  shouldReset: false,
-  setShouldReset: (shouldReset) => set((state) => ({ ...state, shouldReset })),
-  setDefaultContent: (defaultContent) =>
-    set((state) => ({ ...state, defaultContent: defaultContent })),
-  setTweets: (tweets) => set((state) => ({ ...state, tweets })),
-  setDate: (date) => set((state) => ({ ...state, date })),
-  setTime: (time) => set((state) => ({ ...state, time })),
-  setContent: (content) => set((state) => ({ ...state, content })),
-  reset: () => set(() => initialState),
-}));
+export const useStore = createWithEqualityFn<State>(
+  (set) => ({
+    ...initialState,
+    shouldReset: false,
+    setShouldReset: (shouldReset) =>
+      set((state) => ({ ...state, shouldReset })),
+    setDefaultContent: (defaultContent) =>
+      set((state) => ({ ...state, defaultContent: defaultContent })),
+    setTweets: (tweets) => set((state) => ({ ...state, tweets })),
+    setDate: (date) => set((state) => ({ ...state, date })),
+    setTime: (time) => set((state) => ({ ...state, time })),
+    setContent: (content) => set((state) => ({ ...state, content })),
+    reset: () => set(() => initialState),
+  }),
+  shallow
+);
