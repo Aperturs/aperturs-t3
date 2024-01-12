6c63dd14d5b3f1b1f5f6e8a23f2d66a62b7990d1
swaraj bachu <rajswaraj.r@gmail.com>
2024-01-12 21:16:45 +0530

    for now just making it run

--- src/components/layouts/sidebar/accordianMenu.tsx
+++ src/components/layouts/sidebar/accordianMenu.tsx
@@ -9,7 +9,7 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 interface AccordanceProps {
--- src/pages/api/post/schedule.ts
+++ src/pages/api/post/schedule.ts
@@ -3,21 +3,18 @@ import { appRouter } from "~/server/api/root";
 import cronJobServer from "~/server/cronjob";
 import { prisma } from "~/server/db";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const id = req.query.id as string;
   const userId = req.query.userId as string;
 
   console.log(id);
-  const caller = appRouter.createCaller({
-    prisma: prisma,
-    cronJobServer: cronJobServer,
-    currentUser: userId,
-  });
+  // const caller = appRouter.createCaller({
+  //   prisma: prisma,
+  //   cronJobServer: cronJobServer,
+  //   currentUser: userId,
+  // });
 
-  const response = await caller.post.postByPostId({ postId: id });
+  // const response = await caller.post.postByPostId({ postId: id });
 
-  res.status(200).json({ message: response });
+  res.status(200).json({ message: "nothing for now" });
 }
