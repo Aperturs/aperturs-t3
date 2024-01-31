b3084fab8477861de6ef58ac9a528b68a1575c94
swaraj bachu <rajswaraj.r@gmail.com>
2024-01-31 11:57:40 +0530

    some routing

--- src/app/api/s3-upload/route.ts
+++ src/app/api/s3-upload/route.ts
@@ -1,1 +1,9 @@
+import { APIRoute, sanitizeKey } from "next-s3-upload";
+
 export { POST } from "next-s3-upload/route";
+
+export default APIRoute.configure({
+  key(req, filename) {
+    return `post/${sanitizeKey(filename)}`;
+  },
+});
