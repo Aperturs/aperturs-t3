4bb8611f38a76dedaa2d2f552b692ec1fa791dff
swaraj bachu <rajswaraj.r@gmail.com>
2024-02-15 15:48:43 +0530

    tooling

---
+++ tooling/eslint/nextjs.js
@@ -0,0 +1,26 @@
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  extends: ["plugin:@next/next/core-web-vitals"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        checksVoidReturn: false,
+      },
+    ],
+  },
+};
+
+module.exports = config;
+
