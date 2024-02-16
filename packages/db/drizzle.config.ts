dc4c64f6d9d56b617cea4efff031bfeca965915a
Tejas013 <tejasmadhukar6@gmail.com>
2024-02-16 21:08:33 +0530

    Updated drizzle config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 
-export default defineConfig({
+const uri = [
+  "mysql://",
+  process.env.DATABASE_USERNAME,
+  ":",
+  process.env.DATABASE_PASSWORD,
+  "@",
+  process.env.DATABASE_HOST,
+  ":3306/",
+  process.env.DATABASE_NAME,
+  '?ssl={"rejectUnauthorized":true}',
+].join("");
+
+export default {
   schema: "./src/schema/*",
   driver: "mysql2",
-  dbCredentials: { uri: process.env.DATABASE_URL + "" },
-  verbose: true,
-  strict: true,
-  tablesFilter: ["t3turbo_*"],
-});
+  dbCredentials: { uri },
+} satisfies Config;
