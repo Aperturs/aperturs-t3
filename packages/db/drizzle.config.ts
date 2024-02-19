b494f22de761f7ca7817a76fa88381e29d1a356e
Tejas013 <tejasmadhukar6@gmail.com>
2024-02-19 11:01:33 +0530

    changed driver

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,19 +1,8 @@
 import type { Config } from "drizzle-kit";
 
-const uri = [
-  "mysql://",
-  process.env.DATABASE_USERNAME,
-  ":",
-  process.env.DATABASE_PASSWORD,
-  "@",
-  process.env.DATABASE_HOST,
-  ":3306/",
-  process.env.DATABASE_NAME,
-  '?ssl={"rejectUnauthorized":true}',
-].join("");
-
 export default {
   schema: "./src/schema/*",
-  driver: "mysql2",
-  dbCredentials: { uri },
+  driver: "pg",
+  out: "./drizzle",
+  dbCredentials: { connectionString: process.env.DATABASE_URL + "" },
 } satisfies Config;
