80a5a9fa78e45a190fc741c8711e139a4186798b
Swaraj <swarajbachu@users.noreply.github.com>
2024-03-09 21:58:35 +0530

    cdk for yt

---
+++ apps/cdk-infra-yt/test/cdk-aperturs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAperturs from '../lib/cdk-aperturs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-aperturs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAperturs.CdkApertursStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
