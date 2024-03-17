f4cdd19d8df9ec79f1baba073ef701109ff198b1
swaraj bachu <rajswaraj.r@gmail.com>
2024-03-17 20:56:17 +0530

    lint fix

--- packages/ui/src/components/ui/use-toast.ts
+++ packages/ui/src/components/ui/use-toast.ts
@@ -126,6 +126,7 @@ export const reducer = (state: State, action: Action): State => {
   }
 };
 
+// eslint-disable-next-line @typescript-eslint/array-type
 const listeners: Array<(state: State) => void> = [];
 
 let memoryState: State = { toasts: [] };
